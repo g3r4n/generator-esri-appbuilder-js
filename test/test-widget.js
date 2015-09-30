8294eef1e62c21862e190b3cec623139a10ab176
Enguerrand des Vaux <enguerrand.des-vaux@capgemini.com>
2015-09-30 19:00:42 +0200

    MAJ test

--- test/test-widget.js
+++ test/test-widget.js
@@ -3,14 +3,14 @@
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('esri-appbuilder-js:widget subgenerator', function () {
+describe('	wab:widget subgenerator', function () {
   beforeEach(function (done) {
     helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
       if (err) {
         return done(err);
       }
 
-      this.widget = helpers.createGenerator('esri-appbuilder-js:widget', [
+      this.widget = helpers.createGenerator('wab:widget', [
         '../../widget'
       ]);
       console.log('cleared folder and created widget generator');
